b3ed497241f6a8e29b9749563b55108189311b7b
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-04-27 23:39:02 -0500

    Added Catalog list and renderList

--- scripts.js
+++ scripts.js
@@ -1,1 +1,48 @@
+const ul = document.body.appendChild(document.createElement('ul'));
 
+const catalog = [
+  {
+    category: 'Sporting Goods',
+    price: '$49.99',
+    stocked: true,
+    name: 'Football',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$9.99',
+    stocked: true,
+    name: 'Baseball',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$29.99',
+    stocked: false,
+    name: 'Basketball',
+  },
+  {
+    category: 'Electronics',
+    price: '$99.99',
+    stocked: true,
+    name: 'iPod Touch',
+  },
+  {
+    category: 'Electronics',
+    price: '$399.99',
+    stocked: false,
+    name: 'iPhone 5',
+  },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+];
+
+function renderList(listItems) {
+  ul.innerHTML = listItems
+    .map(
+      ({ price }) =>
+        `<li>
+  <label">${price}</label>
+  </li>`,
+    )
+    .join('');
+}
+
+renderList(catalog);
